b63022d816acb5956a79b7ea3d690756317e036a
serkankaragoz <serkankaragoz2002@gmail.com>
2022-06-12 13:23:39 +0300

    My first Commit try with using Intellij Idea

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Helloo");
+        // VCS -> Enable Versio Control Integration -> Git
+        // Version Control Shortcut: Ait+9
+
+    }
+}
